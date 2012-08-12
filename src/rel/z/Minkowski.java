bb0df35fc4a2ae26f3a92f2ba99fe1405f62e4c5
Zach Bailey <zlbjux@gmail.com>
2012-08-11 22:31:50 -0700

    It hit Yan like a tom

--- src/rel/z/Minkowski.java
+++ src/rel/z/Minkowski.java
@@ -1,5 +1,26 @@
 package rel.z;
 
 public class Minkowski {
+	
+	
+	double x1(double x)
+	{
+		return x;
+	}
+	
+	double x2(double y)
+	{
+		return y;
+	}
+	
+	double x3(double z)
+	{
+		return z;
+	}
+	
+	double x4(double c, double t)
+	{
+		return Math.sqrt(-1) * c*t;
+	}
 
 }
