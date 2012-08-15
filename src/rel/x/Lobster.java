09e04fb04cb818ca9efbbe9218ea9566a889017b
Zach Bailey <zach@poplicus.com>
2012-08-15 09:42:41 -0700

    forgot get/set

--- src/rel/x/Lobster.java
+++ src/rel/x/Lobster.java
@@ -9,8 +9,7 @@ package rel.x;
  *
  */
 public class Lobster 
-{
-	
+{	
 	private double m;
 	private double v;
 	private double r;
@@ -89,4 +88,116 @@ public class Lobster
 	{
 		System.exit(0);
 	}
+	
+	
+	
+	public double getM() {
+		return m;
+	}
+
+
+	public void setM(double m) {
+		this.m = m;
+	}
+
+
+	public double getV() {
+		return v;
+	}
+
+
+	public void setV(double v) {
+		this.v = v;
+	}
+
+
+	public double getR() {
+		return r;
+	}
+
+
+	public void setR(double r) {
+		this.r = r;
+	}
+
+
+	public double getX() {
+		return x;
+	}
+
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+
+	public double getY() {
+		return y;
+	}
+
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+
+	public double getZ() {
+		return z;
+	}
+
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+
+
+	public double getT() {
+		return t;
+	}
+
+
+	public void setT(double t) {
+		this.t = t;
+	}
+
+
+	public int getK() {
+		return k;
+	}
+
+
+	public void setK(int k) {
+		this.k = k;
+	}
+
+
+	public double getxSlope() {
+		return xSlope;
+	}
+
+
+	public void setxSlope(double xSlope) {
+		this.xSlope = xSlope;
+	}
+
+
+	public double getySlope() {
+		return ySlope;
+	}
+
+
+	public void setySlope(double ySlope) {
+		this.ySlope = ySlope;
+	}
+
+
+	public double getzSlope() {
+		return zSlope;
+	}
+
+
+	public void setzSlope(double zSlope) {
+		this.zSlope = zSlope;
+	}
+	
 }
