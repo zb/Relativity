3a4e6a4b88f8396b57c9c671be2d2610d1ac1b04
Zach Bailey <zlbjux@gmail.com>
2012-08-28 22:55:10 -0700

    Proper K

--- src/rel/K.java
+++ src/rel/K.java
@@ -4,18 +4,21 @@ import java.util.List;
 
 import rel.x.GodsFishTank;
 import rel.x.Lobster;
+import rel.z.Lorentz;
 
 public class K 
 {
-	private double x;
-	private double y;
-	private double z;
+	private double gx;
+	private double gy;
+	private double gz;
+	private double gt;
+	
+	private double lx;
+	private double ly;
+	private double lz;
+	private double lt;
 
-	private double gftT;
-	private double lT;
-
-	private double k;
-	private double K;
+	private int k;
 	
 	private GodsFishTank gft;
 	private Lobster l;
@@ -48,8 +51,8 @@ public class K
 		
 		this.lv = l.getV();
 		
-		this.gftT = gft.getT();
-		this.lT = l.getT();
+		this.gt = gft.getT();
+		this.lt = l.getT();
 		
 		this.kineticEnergyOfGft = gft.getKineticEnergy();
 		this.kineticEnergyOfLobster = l.getKineticEnergy();
@@ -61,46 +64,104 @@ public class K
 		k.setK(k.getK() + 1);		
 		
 		this.gft = k.getGft();
+		this.gv = gft.getV();
 		
-		this.gft.setX(gft.getX() + gft.getXSlope() );
-		this.gft.setY(gft.getY() + gft.getYSlope() );
-		this.gft.setZ(gft.getZ() + gft.getZSlope() );
+		this.gx = gft.getX();
+		this.gy = gft.getY();
+		this.gz = gft.getZ();
+		this.gt = gft.getT();
+		
+		gft.setX(Lorentz.first(gx, gv, gt));
+		gft.setY(Lorentz.second(gy));
+		gft.setZ(Lorentz.third(gz));
+		gft.setT(Lorentz.fourth(gx, gv, gt));
 		
 		this.l = this.gft.getL();
+		this.lv = this.l.getV();
 		
-		this.l.setX(l.getX() + l.getXSlope());
-		this.l.setY(l.getY() + l.getYSlope());
-		this.l.setZ(l.getZ() + l.getZSlope());		
-	}
+		this.lx = l.getX();
+		this.ly = l.getY();
+		this.lz = l.getZ();
+		this.lt = l.getT();
+		
+		l.setX(Lorentz.first(lx,  lv, lt));
+		l.setY(Lorentz.second(ly));
+		l.setZ(Lorentz.third(lz));	
+		l.setT(Lorentz.fourth(lx,  lv, lt));
+	}	
 	
-	
-	
-	
-	public double getX() {
-		return x;
-	}
-	public void setX(double x) {
-		this.x = x;
-	}
-	public double getY() {
-		return y;
-	}
-	public void setY(double y) {
-		this.y = y;
-	}
-	public double getZ() {
-		return z;
-	}
-	public void setZ(double z) {
-		this.z = z;
-	}
 
 
-	public double getK() {
-		return K;
+
+	public double getGx() {
+		return gx;
 	}
-	public void setK(double k) {
-		K = k;
+
+	public void setGx(double gx) {
+		this.gx = gx;
+	}
+
+	public double getGy() {
+		return gy;
+	}
+
+	public void setGy(double gy) {
+		this.gy = gy;
+	}
+
+	public double getGz() {
+		return gz;
+	}
+
+	public void setGz(double gz) {
+		this.gz = gz;
+	}
+
+	public double getGt() {
+		return gt;
+	}
+
+	public void setGt(double gt) {
+		this.gt = gt;
+	}
+
+	public double getLx() {
+		return lx;
+	}
+
+	public void setLx(double lx) {
+		this.lx = lx;
+	}
+
+	public double getLy() {
+		return ly;
+	}
+
+	public void setLy(double ly) {
+		this.ly = ly;
+	}
+
+	public double getLz() {
+		return lz;
+	}
+
+	public void setLz(double lz) {
+		this.lz = lz;
+	}
+
+	public double getLt() {
+		return lt;
+	}
+
+	public void setLt(double lt) {
+		this.lt = lt;
+	}
+
+	public int getK() {
+		return k;
+	}
+	public void setK(int k) {
+		this.k = k;
 	}
 	public GodsFishTank getGft() {
 		return gft;
